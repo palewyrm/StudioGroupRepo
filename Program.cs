c368d5fd5af1131088ee1e604bc7473f5e343837
paul-study <paulstudylaptop@gmail.com>
2025-09-16 10:56:56 +1200

    added age input

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Welcome to Studio 1 BlackJack!");
+
+            Console.WriteLine("Are you over 18 years old? type y or n");
+            char ageInput = Convert.ToChar(Console.ReadLine());
+
+            if (ageInput == 'y')
+            {
+                Console.WriteLine("");
+
+
+            }
+            else 
+            {
+                Console.WriteLine("You are to young to play!!!!");
+            }
+
         }
+
+        
+
     }
 }
